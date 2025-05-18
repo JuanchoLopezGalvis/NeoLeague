7859d7e133a53c30edfc2906d881b4ee10d3df83
jugov <jugov@ASUS-JDGV1610>
2025-05-18 15:56:07 -0500

    fds

--- NeoLeagueArena/src/co/edu/unbosque/model/persistence/AdministradorDAO.java
+++ NeoLeagueArena/src/co/edu/unbosque/model/persistence/AdministradorDAO.java
@@ -66,7 +66,7 @@ public class AdministradorDAO implements OperacionDAO<AdministradorDTO, Administ
 	@Override
 	public boolean delete(AdministradorDTO toDelete) {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
